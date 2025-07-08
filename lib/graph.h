9d801a95e493cc99fa622075ea03853a00cf625f
itznokx <dimitri.b.medeiros@gmail.com>
2025-07-08 12:50:05 -0300

    graph header

--- lib/graph.h
+++ lib/graph.h
@@ -22,4 +22,31 @@ typedef struct graph{
 	listedge* edge_list;
 }graph;
 
+
+int insert_vertex (graph* g,node* n);
+
+int remove_vertex_by_value (graph* g, TYPE_VAL v);
+
+int remove_vertex_by_label (graph* g, char* l);
+
+int insert_edge (graph* g, node* a, node* b);
+
+int remove_edge (graph* g, node* a, node* b);
+
+int remove_edge_by_value (graph* g, node* a, TYPE_VAL v);
+
+int remove_edge_by_label (graph* g, node* a, char* l);
+
+int remove_edge_value_to_value (graph* g, TYPE_VAL a, TYPE_VAL b);
+
+void print_vertexes (graph* g);
+
+void print_edges (graph* g);
+
+void print_vertex_adj (graph* g, TYPE_VAL v);
+
+void free_vertex_list (graph* g);
+
+void free_edge_lest (graph* g);
+
 #endif
