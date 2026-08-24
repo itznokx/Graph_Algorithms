e458881d68b74332b61f6c337b8df59561f25706
itznokx <dimitri.b.medeiros@gmail.com>
2026-08-24 10:38:51 -0300

    chore(test_node.c): remove node x neighbourhood print in node test file

--- test_node.c
+++ test_node.c
@@ -12,11 +12,9 @@ int main (int narg,char* argc[]){
 	printf("Inserted c\n");
 	insert_neighbour(a,d);
 	printf("Inserted d\n");
-	printf("A neighbourhood\n");
 	print_node_adj(a);
 	insert_neighbour(e,c);
 	insert_neighbour(e,a);
-	printf("E neighbourhood\n");
 	print_node_adj(e);
 	free_adj(a);
 	free_adj(b);
